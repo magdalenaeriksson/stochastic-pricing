e8b78a659cfeea9a51f594e6633812ace0084dca
Magdalena Eriksson <magdalena.erk@gmail.com>
2025-12-14 11:55:46 +0100

    added simulation functions

--- src/simulation.py
+++ src/simulation.py
@@ -2,17 +2,99 @@
 
 import numpy as np
 
-# simulate Geometric Brownian Motion price paths using Euler–Maruyama method
-def simulate_gbm(mu=0.05, sigma=0.2, S0=100, T=1.0, dt=1/252, n_paths=5):
+def simulate_gbm(params, seed=None):
+    # simulate geometric brownian motion (GBM) price paths:
+    #
+    # dS = mu * S * dt + sigma * S * dW
+    #   =>   S_{t+1} = S_t * exp{(mu - sigma²/2)dt + sigma*sqrt{dt}*z_t}
+    #
+    # S is the asset price at time t, 
+    # W a Wiener process (Brownian motion),
+    # z one realization of the the Brownian increment dW ≈ sqrt{dt}*z_t (one draw from normal distr.), and
+    # a random seed for reproducibility
+
+    # return params are:
+    # times : array of time points
+    # S     : simulated price paths, shape (n_steps, n_paths)
+    mu, sigma, S0, T, dt, n_paths = (params.mu, params.sigma, params.S0, params.T, params.dt, params.n_paths)
+    rng = np.random.default_rng(seed)
+
+    n_steps = int(T / dt) 
+    times = np.linspace(0, T, n_steps+1)
+    S = np.zeros((n_steps+1, n_paths))
+    S[0] = S0
+
+    for t in range(1, n_steps+1):
+        z = rng.standard_normal(n_paths)
+        S[t] = S[t-1] * np.exp((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * z)
+
+    return times, S
+
+def simulate_ou(params, seed=None):
+    theta, mu, sigma, X0, T, dt, n_paths = (params.theta, params.mu, params.sigma, params.X0, params.T, params.dt, params.n_paths)
+    rng = np.random.default_rng(seed)
 
     n_steps = int(T / dt)
+    times = np.linspace(0, T, n_steps+1)
+    X = np.zeros((n_steps+1, n_paths))
+    X[0] = X0
+
+    for t in range(1, n_steps+1):
+        z = rng.standard_normal(n_paths)
+        X[t] = X[t-1] + (mu - X[t-1]) * theta * dt + sigma * np.sqrt(dt) * z
+
+    return times, X
+
+def simulate_heston(params, seed=None):
+    # dS = mu * S * dt + sqrt(v) * S * dW_1
+    # dv = kappa * (theta - v) * dt + xi * sqrt(v) * dW_2
+    mu, kappa, theta, xi, rho, S0, v0, T, dt, n_paths = (params.mu, params.kappa, params.theta, params.xi, params.rho, params.S0, params.v0, params.T, params.dt, params.n_paths)
+
+    rng = np.random.default_rng(seed)
+
+    n_steps = int(T / dt) 
+    times = np.linspace(0, T, n_steps+1)
+    S = np.zeros((n_steps+1, n_paths))
+    v = np.zeros_like(S)
+
+    S[0] = S0
+    v[0] = v0
+
+    for t in range(1, n_steps+1):
+        z1 = rng.standard_normal(n_paths)
+        z2 = rng.standard_normal(n_paths)
+        z_corr = rho * z1 + np.sqrt(1 - rho**2) * z2
+        
+        v_prev = np.maximum(v[t-1], 0)
+        v[t] = v_prev + kappa * (theta - v_prev) * dt \
+                + xi * np.sqrt(v_prev) * np.sqrt(dt) * z_corr
+        v[t] = np.maximum(v[t], 0)
+
+        S[t] = S[t-1] + mu * S[t-1] * dt \
+                + np.sqrt(v_prev) * S[t-1] * np.sqrt(dt) * z1
+
+    return times, S, v
+
+def simulate_merton(params, seed=None):
+    # dS = mu * S * dt + sigma * S * dW + J * S * dN
+    # N Poisson rate and jump size J = exp(Y)-1 with Y being 
+    # normally distributed N(mu, sigma²), so that when Y=0
+    # there is no jump, if Y<0, the jump reduces the price, and
+    # if Y>0, the jump boosts the price
+    mu, sigma, S0, lambda_j, mu_j, sigma_j, T, dt, n_paths = (params.mu, params.sigma, params.S0, params.lambda_j, params.mu_j, params.sigma_j, params.T, params.dt, params.n_paths)
+    rng = np.random.default_rng(seed)
+
+    n_steps = int(T / dt) 
     times = np.linspace(0, T, n_steps)
     S = np.zeros((n_steps, n_paths))
     S[0] = S0
 
     for t in range(1, n_steps):
-        z = np.random.standard_normal(n_paths)
-        S[t] = S[t-1] * np.exp((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * z)
+        z   = rng.standard_normal(n_paths)
+        Nj  = rng.poisson(lambda_j * dt, size=n_paths)
+        
+        J = np.exp(mu_j + sigma_j * rng.standard_normal(n_paths))-1
+
+        S[t] = S[t-1] + mu * S[t-1] * dt + sigma * S[t-1] * np.sqrt(dt) * z + S[t-1] * Nj * J # jump to linear order
 
     return times, S
-
