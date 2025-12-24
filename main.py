adf350f191a3f9df410e53feb09b96eeebc0a512
Magdalena Eriksson <magdalena.erk@gmail.com>
2025-12-24 09:56:37 +0100

    removed comparison plot function (see calibration notebook instead)

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# run source .venv/bin/activate to activate virtual environment in terminal from project root
-
+"""
+Main file to simulate stochastic price processes and compare with observed market data.
+"""
 from src.cli import build_parser
 from src.models import GBMParams, OUParams, HestonParams, MertonParams
 from src.simulation import simulate_gbm, simulate_ou, simulate_heston, simulate_merton
-from src.utils import load_prices, realized_variance
-from src.analysis import plot_real_vs_simulated
+from src.utils import load_prices
 
 import matplotlib.pyplot as plt
-import argparse
 import numpy as np
 
 def plot(times, data, name):
-    plt.figure(figsize=(10,5))
+    """ plot simulated price paths """
+    plt.figure(figsize=(8,4))
     for i in range(data.shape[1]):
         plt.plot(times, data[:,i], lw=1.2)
     plt.title(f"{name} simulated paths")
@@ -46,27 +46,18 @@ def run_merton(args):
 RUNNERS = {
     "GBM"   : run_gbm,
     "OU"    : run_ou,
-    "HESTON": run_heston,
-    "MERTON": run_merton
+    "Heston": run_heston,
+    "Merton": run_merton
 }
 
-
-# prices[-1] means "from the end", i.e.
-# prices[0], prices[1] = first and second elements, while
-# prices[-1], prices[-2] = last and second-last elements
-# prices[-N] = last N elements
-# and so S0=prices[-1] means that we start the simulation from 
-# the most recent observed price. So we use historical prices to
-# calibrate the parameters and then simulate forward, from today's
-# price. The simulation then models plausable future price paths.
-
 def get_gbm_params(args):
+    """ set GBM parameters according to either calibrated values or manual input """
     if args.calibrated:
         from src.calibration import calibrate_gbm
 
         prices = load_prices(args.data_path, args.price_col)
 
-        mu_hat, sigma_hat = calibrate_gbm(prices)
+        mu_hat, sigma_hat = calibrate_gbm(prices, args.dt)
         return GBMParams(mu=mu_hat, 
                          sigma=sigma_hat, 
                          S0=prices[-1], 
@@ -78,12 +69,13 @@ def get_gbm_params(args):
                          args.T, args.dt, args.n_paths)
 
 def get_ou_params(args):
+    """ set OU parameters according to either calibrated values or manual input """
     if args.calibrated:
         from src.calibration import calibrate_ou
 
         prices = load_prices(args.data_path, args.price_col)
 
-        theta_hat, mu_hat, sigma_hat = calibrate_ou(np.log(prices),args.dt)
+        theta_hat, mu_hat, sigma_hat = calibrate_ou(np.log(prices), args.dt)
         return OUParams(theta=theta_hat, 
                         mu=mu_hat, 
                         sigma=sigma_hat,
@@ -96,12 +88,13 @@ def get_ou_params(args):
                         args.X0, args.T, args.dt, args.n_paths)
 
 def get_heston_params(args):
+    """ set Heston parameters according to either calibrated values or manual input """
     if args.calibrated:
         from src.calibration import calibrate_gbm, calibrate_heston
 
         prices = load_prices(args.data_path, args.price_col)
-        mu_hat, _ = calibrate_gbm(prices)   # ignore GBM sigma here
-        kappa_hat, theta_hat, xi_hat, rho_hat, v0 = calibrate_heston(prices)
+        mu_hat, _ = calibrate_gbm(prices, args.dt)   # ignore GBM sigma here
+        kappa_hat, theta_hat, xi_hat, rho_hat, v0 = calibrate_heston(prices, args.dt)
         return HestonParams(mu=mu_hat,
                             kappa=kappa_hat, 
                             theta=theta_hat,
@@ -118,12 +111,13 @@ def get_heston_params(args):
                             args.v0, args.T, args.dt, args.n_paths)
 
 def get_merton_params(args):
+    """ set Merton parameters according to either calibrated values or manual input """
     if args.calibrated:
         from src.calibration import calibrate_merton
 
         prices = load_prices(args.data_path, args.price_col)
         
-        mu_hat, sigma_hat, lambda_j_hat, mu_j_hat, sigma_j_hat = calibrate_merton(prices)
+        mu_hat, sigma_hat, lambda_j_hat, mu_j_hat, sigma_j_hat = calibrate_merton(prices, args.dt)
         return MertonParams(mu=mu_hat,
                             sigma=sigma_hat,
                             S0=prices[-1],
@@ -138,41 +132,13 @@ def get_merton_params(args):
                             args.lambda_j, args.mu_j, args.sigma_j,
                             args.T, args.dt, args.n_paths)
 
-def compare_real_vs_simulated(model, args, time_window:int): 
-    price_data = load_prices(args.data_path, args.price_col)
-    price_window = price_data[-time_window:] # time_window is in number of days 
-
-    if model == "GBM":
-        params = get_gbm_params(args)
-        _ , S = simulate_gbm(params, seed=42)
-    elif model == "OU":
-        params = get_ou_params(args)
-        _ , X = simulate_ou(params, seed=42)
-        S = np.exp(X)
-    elif model == "HESTON":
-        params = get_heston_params(args)
-        _ , S, v = simulate_heston(params, seed=42)
-    elif model == "MERTON":
-        params = get_merton_params(args)
-        _ , S = simulate_merton(params, seed=42)
-    
-    plot_real_vs_simulated(price_window, S, model)
-
 
 def main():
     
     parser = build_parser()
     args = parser.parse_args()
-
-    model = args.model.upper()
-
-    if model not in RUNNERS:
-        #raise ValueError(f"unknown model '{args.model}' -- try GBM, OU, Heston, or Merton")
-        print(f"unknown model '{args.model}' -- try GBM, OU, Heston, or Merton")
-        exit()
     
-    RUNNERS[model](args)
-    compare_real_vs_simulated(model, args, 252)
+    RUNNERS[args.model](args)
 
 
 if __name__ == "__main__":
